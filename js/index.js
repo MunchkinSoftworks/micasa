28027f026b6d7a7c20f206f83da606c4d41a55aa
MunchkinSoftworks <46210264+MunchkinSoftworks@users.noreply.github.com>
2019-04-14 19:12:39 -0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,17 +1,35 @@
-//alert("hello world");
-$(".waypoint--1").waypoint(function(direction){
-    if(direction == "down"){
-        //alert("hellow worlds");
-        $('.waypoint--1').addClass('fixed-nav');
-    }
-}, {
-    offset: '65%'
+$(document).ready(function (){
+    $(".smooth-scroll").click(function(event){
+        event.preventDefault();
+        var section = $(this).attr("href");
+        $('html, body').animate({
+            scrollTop: $(section).offset().top + 120
+        }, 2000);
+    });
+    //smooth-scroll-home
+    $(".smooth-scroll-home").click(function(event){
+        event.preventDefault();
+        var section = $(this).attr("href");
+        $('html, body').animate({
+            scrollTop: $(section).offset().top 
+        }, 2000);
+    });
 });
-$(".waypoint--2").waypoint(function(direction){
-    if(direction == "down"){
-        //alert("hellow worlds");
-        $('.waypoint--2').addClass('fadeIn-center');
-    }
-}, {
-    offset: '65%'
-});
+//alert("hello world"); 
+//$(".waypoint--1").waypoint(function(direction){
+//    if(direction == "down"){
+//        //alert("hellow worlds");
+///        $('.waypoint--1').addClass('fixed-nav');
+//    }
+//}, {
+//    offset: '65%'
+///});
+//$(".waypoint--2").waypoint(function(direction){
+//    if(direction == "down"){
+///        //alert("hellow worlds");
+///        $('.waypoint--2').addClass('fadeIn-center');
+//    }
+//}, {
+//    offset: '65%'
+//});
+
